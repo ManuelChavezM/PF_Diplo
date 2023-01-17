e69fbe02c21d1c4100b17b4c01356e6345295d0d
Hayabusha117 <62165295+Hayabusha117@users.noreply.github.com>
2023-01-17 17:59:49 -0600

    UPDATE-Implementacion First Person

--- Assets/Scripts/FPController.cs
+++ Assets/Scripts/FPController.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class FPController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed;
+    public float speedRotation;
+    public Transform cameraTransform;
+    private Rigidbody fprb;
+    private Vector3 movement;
+
+    private float yVel;
+    private bool locked;
+
+    public float jumpForce = 5f;
+    private void Start()
     {
-        
+        //se le indica que traiga el componente rgidbody 
+        fprb = GetComponent<Rigidbody>();
+        locked = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void UnlockControll()
     {
-        
+        locked = false;
     }
+
+    private void Update()
+    {
+        if (locked == true)
+        {
+            return;
+        }
+
+        movement = Vector3.zero;
+
+        yVel = fprb.velocity.y;
+
+        //Movimiento del jugador con el teclado
+        /* transform.Translate(Vector3.forward * speed * Time.deltaTime * Input.GetAxis("Vertical"));
+         transform.Translate(Vector3.right * speed * Time.deltaTime * Input.GetAxis("Horizontal"));*/
+
+        transform.Rotate(Vector3.up * speedRotation * Time.deltaTime * Input.GetAxis("Mouse X"));
+
+        //Formas de rotar el transform de la camera
+        /*cameraTransform.Rotate(Vector3.right * speedRotation * -Input.GetAxis("Mouse Y"));*/
+
+        //transform.GetChild(0).Rotate(Vector3.right * speedRotation * -Input.GetAxis("Mouse Y"));
+
+        // Camera.main.transform.Rotate(Vector3.right * speedRotation * Time.deltaTime * -Input.GetAxis("Mouse Y"));
+
+
+
+        /*movement = transform.forward * speed * Time.deltaTime * Input.GetAxis("Vertical");
+        movement += transform.right * speed * Time.deltaTime * Input.GetAxis("Horizontal");*/
+
+        movement = Camera.main.transform.forward * speed * Time.deltaTime * Input.GetAxis("Vertical");
+        movement += Camera.main.transform.right * speed * Time.deltaTime * Input.GetAxis("Horizontal");
+
+        movement.y = yVel;
+        fprb.velocity = movement;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            fprb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+    }
+
 }
