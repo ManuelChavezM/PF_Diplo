62e9ec44ad5add4377f9ac27f8937c2b16d209e0
Hayabusha117 <62165295+Hayabusha117@users.noreply.github.com>
2023-01-24 15:52:28 -0600

    modeloArma

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform originBullet;
+    public float bulletForce;
+    private GameObject tmpBullet;
+
+    public int Municion;
+
+    private void Start()
+    {
+        Municion = 5;
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Municion != 0)
+            {
+                tmpBullet = Instantiate(bullet, originBullet.position, Quaternion.identity);
+
+                //orientacion de la bala por medio del uso del transform originbullet
+                tmpBullet.transform.up = originBullet.transform.forward;
+
+                //se agrega la fuerzo a la bala
+                tmpBullet.GetComponent<Rigidbody>().AddForce(originBullet.forward * bulletForce, ForceMode.Impulse);
+
+                Municion -= 1;
+            }
+
+
+        }
+    }
+}
