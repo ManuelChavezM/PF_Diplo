ede8616af1d4298fa3cf6659be5f69aaf80d3c0d
Hayabusha117 <62165295+Hayabusha117@users.noreply.github.com>
2023-01-24 18:35:03 -0600

    Cargador

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject explotion;
+
+
+    //Metodo para detectar colisiones, recibe un objeto tipo colision
+    void OnCollisionEnter(Collision collision)
     {
-        
+        //instanciar la explosion en el punto donde esta haciendo colision para que los ejes sean correctos con respecto al vector que detecta la colision
+        //collision.contacts[0].point detecta el primer punto de contacto
+
+        //Instantiate(explotion, collision.contacts[0].point + collision.contacts[0].normal * 0.001f, Quaternion.LookRotation(collision.contacts[0].normal * -1));
+
+
+        Instantiate(explotion, collision.contacts[0].point + collision.contacts[0].normal * 0.001f, Quaternion.LookRotation(collision.contacts[0].normal * -1), collision.transform);
+
+
+
+        //de esta forma nos aseguramos que se destruya el objeto
+        Destroy(this.gameObject);
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
