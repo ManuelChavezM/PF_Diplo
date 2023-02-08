c68082d262d675901c19aa40e106d806b42e0143
Hayabusha117 <62165295+Hayabusha117@users.noreply.github.com>
2023-02-07 19:00:35 -0600

    update- script GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instanceGameManager;
+    public GameObject panelGamplay;
+    
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        instanceGameManager = this;   
     }
 
     // Update is called once per frame
